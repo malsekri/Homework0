0975580590758e09513b9e0ab80b7d0742f2b917
malsekri <mohamed.alsekri@gmail.com>
2022-10-25 10:09:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("The two roots of the quadratic equation x**2 + 2x - 5 are:")
+
+print((-2+(2**2-4*1*(-5))**0.5)/(2*1))
+
+print((-2-(2**2-4*1*(-5))**0.5)/(2*1))
+
+print("Have a good day,")
+print("Mohamed")
